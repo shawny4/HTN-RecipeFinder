0120b72b331e85cf1a69277788047b43d60d117e
ShawnY <145298979+shawny004@users.noreply.github.com>
2024-09-14 10:56:30 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -11,17 +11,39 @@ function App() {
   const [message, setMessage] = useState('');
   const [inputData, setInputData] = useState('');  // New state for form input
 
+  // useEffect(() => {
+  //   getFromFlask().then(responseData => setData(responseData))
+  //   .catch(error => setMessage('Error fetching data from Flask: ' + error));
+  // }, []);
   useEffect(() => {
-    getFromFlask().then(responseData => setData(responseData))
-    .catch(error => setMessage('Error fetching data from Flask: ' + error));
-  }, []);
-  
+    // async function fetchData() {
+    //   try {
+    //     const responseData = await getFromFlask();  // Call the API
+    //     setDataString(JSON.stringify(responseData[0]).replace(/"/g, ''));  // Store the stringified data in the state
+    //     setImageString(JSON.stringify(responseData[2]).replace(/"/g, ''));  // Store the stringified data in the state
+    //   } catch (error) {
+    //     console.error('Error fetching data:', error);
+    //     setDataString('Error fetching data');  // Handle error case by setting an error message
+    //   }
+    // }
+
+//fetchData();  // Call the function to fetch data
+
+
+// GET request using fetch inside useEffect React hook
+fetch('http://127.0.0.1:5000/')
+    .then(response => response.text())
+    .then(data => setData(data));
+    
+}, []);
+
   return (
     <div className="App">
       <Header></Header>
       <header className="content-app">
-        
-        <RecipeList></RecipeList>
+        <p>hello world</p>
+        {/* <RecipeList></RecipeList> */}
+        {data}
       </header>
     </div>
   );
--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,13 +1,44 @@
-import React from 'react';
 import RecipeItem from './RecipeItems.js';
 import './styles/RecipeList.css';  
+import React, { useState, useEffect } from 'react';
+import { getFromFlask } from './api.js';
+import image from './images/test.png';
 
 function RecipeList() {
+    const [data, setData] = useState(null);
+    const [message, setMessage] = useState('');
+    const [dataString, setDataString] = useState('');  // New state for form input
+    const [imageString, setImageString] = useState('');  // New state for form input
+
+    useEffect(() => {
+            // async function fetchData() {
+            //   try {
+            //     const responseData = await getFromFlask();  // Call the API
+            //     setDataString(JSON.stringify(responseData[0]).replace(/"/g, ''));  // Store the stringified data in the state
+            //     setImageString(JSON.stringify(responseData[2]).replace(/"/g, ''));  // Store the stringified data in the state
+            //   } catch (error) {
+            //     console.error('Error fetching data:', error);
+            //     setDataString('Error fetching data');  // Handle error case by setting an error message
+            //   }
+            // }
+    
+        //fetchData();  // Call the function to fetch data
+
+
+        // GET request using fetch inside useEffect React hook
+        fetch('http://127.0.0.1:5000/recipe')
+            .then(response => response.json())
+            .then(data => setData(data));
+            
+    }, []);
+
+
+    
     return (
         <div className="RecipeList">
-            <RecipeItem />
-            <RecipeItem />
-            <RecipeItem />
+            {/* {data.map(item => (<RecipeItem title = {dataString} imgurl = {imageString}/>))}
+            <RecipeItem title = "title" imgurl={image}></RecipeItem> */}
+            {JSON.stringify(data)}
         </div>
     );
 }
