cc9ac249667ec3b25e1056ec97632c5adf6b0fbb
ShawnY <145298979+shawny004@users.noreply.github.com>
2024-09-14 11:09:32 -0400

    create

--- src/App.js
+++ src/App.js
@@ -6,7 +6,15 @@ import './styles/App.css';
 
 function App() {
   
-  
+  useEffect(() => {
+    // GET request using fetch inside useEffect React hook
+    fetch('http://127.0.0.1:5000/')
+        .then(response => response.text())
+        .then(data => console.log(data));
+
+// empty dependency array means this effect will only run once (like componentDidMount in classes)
+}, []);
+
   return (
     <div className="App">
       <header className="content">
